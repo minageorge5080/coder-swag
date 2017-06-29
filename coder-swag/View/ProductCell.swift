0d18ae42b610acad29b682939bbf196527d4e5f5
Mark Price <mark@devslopes.com>
2017-06-29 14:13:23 -0700

    Collection view cell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Mark Price on 6/29/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
